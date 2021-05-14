2bdb0b14b5738d6b5c27e8c26cb7dd46fb920172
Franz Andel <franz.andel@amarbank.co.id>
2021-05-14 22:30:31 +0700

    feat(settings-fragment): Create Settings Preferences Page

--- app/src/main/java/com/franzandel/dicodingexpertsubmission/core/extension/ContextExtension.kt
+++ app/src/main/java/com/franzandel/dicodingexpertsubmission/core/extension/ContextExtension.kt
@@ -1,7 +1,9 @@
 package com.franzandel.dicodingexpertsubmission.core.extension
 
+import android.app.TimePickerDialog
 import android.content.Context
 import android.content.Intent
+import java.util.*
 
 /**
  * Created by Franz Andel on 28/04/21.
@@ -12,5 +14,23 @@ fun Context.goTo(clazz: Class<*>, bundle: (Intent.() -> Unit)? = null) {
     Intent(this, clazz).run {
         bundle?.invoke(this)
         startActivity(this)
+    }
+}
+
+fun Context.showTimePickerDialog(
+    listener: TimePickerDialog.OnTimeSetListener,
+    onCancelListener: (() -> Unit)? = null
+) {
+    val calendar = Calendar.getInstance()
+    val hour = calendar.get(Calendar.HOUR_OF_DAY)
+    val minute = calendar.get(Calendar.MINUTE)
+    val formatHour24 = true
+
+    TimePickerDialog(this, listener, hour, minute, formatHour24).run {
+        setOnCancelListener {
+            onCancelListener?.invoke()
+        }
+
+        show()
     }
 }
